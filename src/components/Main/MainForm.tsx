289b2757794090895f896774fdc14718f72a8b9f
picoloman6 <a2328b@naver.com>
2024-01-24 20:06:41 +0900

    [dev] MainForm label과 input 연결

--- src/components/Main/MainForm.tsx
+++ src/components/Main/MainForm.tsx
@@ -10,14 +10,15 @@ const MainForm = () => {
   return (
     <StForm>
       <StFormTitle>현재 멤버에게 보내고 있습니다</StFormTitle>
-      <label>닉네임</label>
+      <label htmlFor='main-form-name'>닉네임</label>
       <StFormInput
+        id='main-form-name'
         type='text'
         placeholder='이름을 입력하세요'
         autoComplete='none'
       />
-      <label>내용</label>
-      <StFormTextArea placeholder='내용을 입력하세요' />
+      <label htmlFor='main-form-content'>내용</label>
+      <StFormTextArea id='main-form-content' placeholder='내용을 입력하세요' />
       <StFormBtn> 제출</StFormBtn>
     </StForm>
   );
