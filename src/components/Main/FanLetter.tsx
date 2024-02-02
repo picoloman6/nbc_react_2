911c7caaae4bfb41f2a423261156d5fc9bde53fb
picoloman6 <a2328b@naver.com>
2024-02-02 14:57:37 +0900

    [dev] Fanletter.tsx 이벤트 핸들러 명칭 변경, 날짜 변환기 controllers로 이동

--- src/components/Main/FanLetter.tsx
+++ src/components/Main/FanLetter.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { convertDate } from '../../controllers/main';
 import { LetterTypes } from '../../types/mainTypes';
 import { StFanLetterWrapper, StFanLetterName } from './FanLetter.style';
 
@@ -8,31 +8,16 @@ interface FanLetterPropsTypes {
   letter: LetterTypes;
 }
 
-const convertDate = (dateTime: number) => {
-  const dateObj = new Date(dateTime);
-  const year = dateObj.getFullYear();
-  const mon = dateObj.getMonth() + 1;
-  const date = dateObj.getDate();
-  const hour = dateObj.getHours();
-  const min = dateObj.getMinutes();
-
-  return `${year}년 ${mon}월 ${date}일 ${hour}시 ${min}분`;
-};
-
 const FanLetter = ({ letter }: FanLetterPropsTypes) => {
   const navigate = useNavigate();
   const { id, name, content, dateTime } = letter;
 
-  const onClickDetail = (e: React.MouseEvent<HTMLLIElement>) => {
-    const { tagName } = e.target as Element;
-
-    if (tagName !== 'BUTTON') {
-      navigate(`/detail?id=${id}`, { state: letter });
-    }
+  const handleClickDetail = () => {
+    navigate(`/detail?id=${id}`, { state: letter });
   };
 
   return (
-    <StFanLetterWrapper onClick={onClickDetail}>
+    <StFanLetterWrapper onClick={handleClickDetail}>
       <StFanLetterName>{name}</StFanLetterName>
       <span>{convertDate(dateTime)}</span>
       <span>
